67f85ac6b5dcf717064673c395aae8265aa39717
techproed <techproeducation@gmail.com>
2023-03-17 11:52:07 +0300

    Api

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class ApiTest {
+    @Test
+    public void test(){
+
+    assertEquals(200, given().get("https://petstore.swagger.io/v2/pet/10").statusCode());
+
+    }
+}
