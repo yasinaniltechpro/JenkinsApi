0a80270a5b94bd8ebd84c036c5ff3ce123d6a745
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2023-06-13 09:38:49 +0300

    Update ApiTest.java

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -5,12 +5,24 @@ import static org.junit.Assert.assertEquals;
 
 public class ApiTest {
     @Test
-    public void test(){
-          
-        
-    System.out.println("Test çalıştı");
-    assertEquals(200, given().get("https://restful-booker.herokuapp.com/booking").statusCode());
-        given().get("https://restful-booker.herokuapp.com/booking").prettyPrint();
+    public void get01(){
+
+//        i)   Set the URL
+        String url = "https://restful-booker.herokuapp.com/booking/";
+
+//        ii)  Set the expected data
+
+
+//        iii) Send the request and get the response
+        Response response = given().when().get(url);//User sends a GET Request to the url
+        response.prettyPrint();
+
+//        iv)  Do assertion
+        response.
+                then().
+                statusCode(200).//HTTP Status Code should be 200
+                contentType("application/json").//Content Type should be JSON
+                statusLine("HTTP/1.1 200 OK");//Status Line should be HTTP/1.1 200 OK
 
     }
 }
