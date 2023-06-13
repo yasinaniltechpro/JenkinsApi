e52a59b72737593ec276109ad8a09a50370eb222
yasinaniltechpro <105437708+yasinaniltechpro@users.noreply.github.com>
2023-06-13 09:39:58 +0300

    Update ApiTest.java

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,9 +1,25 @@
+package get_requests;
+
+import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
+import static io.restassured.RestAssured.*;
 
 public class ApiTest {
+
+     /*
+        Given
+            https://restful-booker.herokuapp.com/booking/55
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be JSON
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+
     @Test
     public void get01(){
 
